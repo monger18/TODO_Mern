3e5989d9d2ae30e08b8b66811f33666b781cf7e5
monger18 <satyamsri1809@gmail.com>
2024-07-02 12:58:26 +0530

    updating all the routes

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,15 @@
 const mongoose = require('mongoose')
-const { boolean } = require('zod')
 
 mongoose.connect(
   'mongodb+srv://rahulraj18sep200051:VfSLNDTHCH9wzLzS@cluster0.l14wczj.mongodb.net/'
 )
-const todoSchema =
-  mongoose.Schema[
-    {
-      title: String,
-      description: String,
-      completed: Boolean,
-    }
-  ]
+const todoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  completed: Boolean,
+})
 
-const todo = mongoose.model('todo', todoSchema)
+const todos = mongoose.model('todos', todoSchema)
 module.exports = {
-  todo,
+  todos,
 }
--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,43 @@
 const express = require('express')
 const { createTodo, updateTodo } = require('./types')
+const { todos } = require('./db')
 const app = express()
 
 app.use(express.json())
 
-app.post('/todo', function (req, res) {
+app.post('/todo', async function (req, res) {
   const createPayload = req.body
   const parsepayLoad = createTodo.safeParse(createPayload)
   if (!parsepayLoad.success) {
     res.status(411).json({ msg: 'you sent the wrong input' })
     return
   }
+
+  await todos.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  })
+  res.json({ msg: 'Todo created' })
 })
 
-app.get('/todos', function (req, res) {})
+app.get('/todos', async function (req, res) {
+  const todo = await todos.find({})
+  res.json({ todo })
+})
 
-app.put('/completed', function (req, res) {
+app.put('/completed', async function (req, res) {
   const updatePayload = req.body
   const parsepayLoad = updateTodo.safeParse(updatePayload)
   if (!parsepayLoad.success) {
     res.status(411).json({ msg: 'You sent the wrong inputs' })
     return
   }
+
+  await todos.updateOne({ _id: req.body.id }, { $set: { completed: true } })
+  res.json({ msg: 'Todo mark as completed' })
+})
+
+app.listen(3000, function () {
+  console.log('Server started')
 })
